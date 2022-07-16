c0f138d07c91a0d0e6da238c7e47b56eb993f768
Josh Clyde <joshclyde11@gmail.com>
2022-07-15 20:11:40 -0400

    Add a second recipe to useCookbook

--- composables/useCookbook.ts
+++ composables/useCookbook.ts
@@ -1,4 +1,15 @@
-export const useCookbook = () => {
+export const useCookbook = (): Array<{
+  id: string;
+  name: string;
+  ingredients: Array<string>;
+  directions: Array<string>;
+  rating: {
+    josh: 1 | 2 | 3 | 4 | 5;
+    ellie: 1 | 2 | 3 | 4 | 5;
+  };
+  image: string;
+  links: Array<{ name: string; url: string }>;
+}> => {
   /*
     For now, I am not going to feature any editing on the data.
     So just return the object with the data.
@@ -35,6 +46,46 @@ export const useCookbook = () => {
         {
           name: "Original Recipe at Delish",
           url: "https://www.delish.com/cooking/recipe-ideas/recipes/a57645/thai-chicken-lettuce-cups-recipe/",
+        },
+      ],
+    },
+    {
+      id: "spinach_gnocchi_soup",
+      name: "Parmesan Gnocchi Mushroom Soup",
+      ingredients: [
+        "1 tablespoon olive oil",
+        "1 tablespoon butter",
+        "1/2 medium onion chopped",
+        "2 sticks celery chopped finely",
+        "1 tablespoon flour",
+        "3 cloves garlic minced",
+        "16 ounces cremini mushrooms sliced or chopped",
+        "4 cups chicken or veggie broth",
+        "2 dashes Italian seasoning",
+        "1 pound potato gnocchi",
+        "1 cup heavy/whipping cream",
+        "2 cups (packed) fresh baby spinach",
+        "1 cup freshly grated parmesan cheese",
+        "Salt & pepper to taste",
+      ],
+      directions: [
+        "1. Prep your onion and celery and melt the oil & butter in a soup pot over medium-high heat.",
+        "2. Add the onion and celery to the pot and sauté for 5 minutes (it's ok if the onions brown a bit). I like to cut up the mushrooms while it's cooking to save time.",
+        "3. Stir in the flour and cook for about 30 seconds.",
+        "4. Add in the garlic and mushrooms and cook for 5 minutes or so. Once the mushrooms start to release water, give it a good stir to scrape up any brown bits from the bottom of the pot. ",
+        "5. Add the chicken broth, Italian seasoning, and gnocchi to the pot. Increase the heat to high and bring the soup to a boil. Once it's boiling, reduce the heat so it's gently simmering for 5 minutes.",
+        "6. Stir in the cream, spinach, and parmesan cheese. Increase the heat to medium and cook for another 5 minutes or until the spinach is tender and wilted, the cheese melts into the broth, and the broth has thickened up a bit.",
+        "7. Season with salt & pepper as needed. Serve immediately. If you wish, you can grate some extra parmesan cheese on top of the bowls.",
+      ],
+      rating: {
+        josh: 5,
+        ellie: 5,
+      },
+      image: "/images/thai_chicken_lettuce_wraps.png",
+      links: [
+        {
+          name: "Original Recipe",
+          url: "https://www.saltandlavender.com/spinach-gnocchi-soup/",
         },
       ],
     },
--- pages/recipes/[id].vue
+++ pages/recipes/[id].vue
@@ -10,35 +10,41 @@ const recipe = computed(() => {
 <template>
   <div>
     <Link to="/">Home</Link>
-    <Heading1>{{ recipe.name }}</Heading1>
-    <div class="flex">
-      <div class="w-1/3">
-        <Heading2>Ingredients</Heading2>
-        <ul>
-          <li v-for="ingredient in recipe.ingredients">
-            {{ ingredient }}
-          </li>
-        </ul>
+    <Heading1>Timers</Heading1>
+    <Timer></Timer>
+    <template v-if="recipe != null">
+      <Heading1>{{ recipe.name }}</Heading1>
+      <div class="flex">
+        <div class="w-1/3">
+          <Heading2>Ingredients</Heading2>
+          <ul>
+            <li v-for="ingredient in recipe.ingredients">
+              {{ ingredient }}
+            </li>
+          </ul>
+        </div>
+        <div class="w-2/3">
+          <Heading2>Directions</Heading2>
+          <Text v-for="step in recipe.directions">{{ step }}</Text>
+          <Heading2 v-if="recipe.image.length > 0">Images</Heading2>
+          <img class="w-32 h-32" :src="recipe.image" />
+          <Heading2>Ratings</Heading2>
+          <p>Josh: <template v-for="n in recipe.rating.josh">⭐️</template></p>
+          <p>
+            Ellie: <template v-for="n in recipe.rating.ellie">⭐️</template>
+          </p>
+          <Heading2> Links </Heading2>
+          <ul>
+            <li v-for="alksdjf in recipe.links">
+              <Link :to="alksdjf.url">{{ alksdjf.name }}</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="w-2/3">
-        <Heading2>Directions</Heading2>
-        <Text v-for="step in recipe.directions">{{ step }}</Text>
-        <Heading2 v-if="recipe.image.length > 0">Images</Heading2>
-        <img class="w-32 h-32" :src="recipe.image" />
-        <Heading2>Ratings</Heading2>
-        <p>
-          Josh: <template v-for="n in recipe.rating.josh">⭐️</template>
-        </p>
-        <p>
-          Ellie: <template v-for="n in recipe.rating.ellie">⭐️</template>
-        </p>
-        <Heading2> Links </Heading2>
-        <ul>
-          <li v-for="alksdjf in recipe.links">
-            <Link :to="alksdjf.url">{{ alksdjf.name }}</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </template>
+    <template v-else>
+      <Heading1> Not found. </Heading1>
+      <Text> Recipe not found. </Text>
+    </template>
   </div>
 </template>
